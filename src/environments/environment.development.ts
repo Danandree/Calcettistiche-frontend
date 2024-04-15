2226368b587c3a3a4ad21cdd041688fac6ffa705
Danandree <danieleandree91@gmail.com>
2024-04-15 16:37:24 +0200

    commit

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    serverUrl : 'https://calcetistiche-test.oa.r.appspot.com'
+    serverUrl : 'https://calcettistiche.oa.r.appspot.com'
     // serverUrl : 'http://localhost:3000'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    serverUrl : 'https://calcetistiche-test.oa.r.appspot.com'
+    serverUrl : 'https://calcettistiche.oa.r.appspot.com'
     // serverUrl : 'http://192.168.0.4:3000'
 };
