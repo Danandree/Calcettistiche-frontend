1159947f2dab7a5bc3582db1c3dd249fd5d4b78f
Danandree <danieleandree91@gmail.com>
2024-04-18 11:17:03 +0200

    commit

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,4 @@
 export const environment = {
     production: false,
-    serverUrl : 'https://calcettistiche.oa.r.appspot.com'
-    // serverUrl : 'http://localhost:3000'
+    serverUrl : 'http://localhost:3000'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
     production: true,
-    serverUrl : 'https://calcettistiche.oa.r.appspot.com'
-    // serverUrl : 'http://192.168.0.4:3000'
+    serverUrl : 'http://localhost:3000'
 };
